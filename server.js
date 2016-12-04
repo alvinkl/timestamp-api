2645b6f9e7c01ea73366c916c4b81e275e87c250
Alvin Lim <alvinlim@Alvins-MacBook-Pro.local>
2016-12-04 17:19:32 +0700

    added views, style, routes

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 var express = require('express');
+var path = require('path');
+
+const routes = require('./routes/index');
 
 var app = express();
 
-app.get('/', (req, res) => { 
-    res.send('Hello world!');
-})
+/* View Engine Setup */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-app.listen(8080, () => {
-    console.log('App is running on port 3000');
-})
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// app.use((req, res, next) => {
+//   let err = new Error('Not Found!');
+//   err.status = 404;
+//   next(err);
+// });
+
+app.listen(8080, () => console.log('App is running on port 8080'));
+
+module.exports = app;
